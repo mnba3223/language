5a914c22ecd9a028bc3f25d043092b38b75ae890
516 <m516@m516>
2015-11-13 23:49:49 +0800

    YEEE

--- language/wiki/urls.py
+++ language/wiki/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from wiki import views
 
 urlpatterns = [
- url(r'^$', views.wiki, name='wiki'),
- url(r'^about/$', views.about, name='about'),
+    url(r'^$', views.wiki, name='wiki'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<categoryName>[\w\-]+)/$', views.category, name='category'),
  
 ]
